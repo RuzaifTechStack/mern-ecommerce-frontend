fd0fcc09b4927f7987eb569a4392d56430b319ff
mohammed Ruzaif <mohammedruzaif9@gmail.com>
2024-05-28 15:09:00 +0530

    Initial

--- src/components/cart-item.tsx
+++ src/components/cart-item.tsx
@@ -1,21 +1,24 @@
-import { FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { FaTrash } from "react-icons/fa";
 import { server } from "../redux/store";
-import { CartItem } from "../types/types";
+import { CartItem as CartItemtype } from "../types/types";
+
+
 type CartItemProps = {
-  cartItem: CartItem;
-  incrementHandler:(cartItem: CartItem) => void;
-  decrementtHandler:(cartItem: CartItem) => void;
-  removetHandler:(id: string) => void;
+  cartItem: CartItemtype;
+  incrementHandler: (cartItem: CartItemtype) => void;
+  decrementHandler: (cartItem: CartItemtype) => void;
+  removeHandler: (id: string) => void;
 };
 
 const CartItem = ({
-  cartItem, 
-  incrementHandler, 
-  decrementtHandler, 
-  removetHandler}: CartItemProps) => {
+  cartItem,
+  incrementHandler,
+  decrementHandler,
+  removeHandler,
+}: CartItemProps) => {
+  const { photo, productId, name, price, quantity } = cartItem;
 
-  const {photo, productId, name, price, quantity} = cartItem;
   return (
     <div className="cart-item">
       <img src={`${server}/${photo}`} alt={name} />
@@ -23,15 +26,18 @@ const CartItem = ({
         <Link to={`/product/${productId}`}>{name}</Link>
         <span>₹{price}</span>
       </article>
+
       <div>
-        <button onClick={()=> decrementtHandler(cartItem)}>-</button>
+        <button onClick={() => decrementHandler(cartItem)}>-</button>
         <p>{quantity}</p>
-        <button onClick={()=> incrementHandler(cartItem)}>+</button>
+        <button onClick={() => incrementHandler(cartItem)}>+</button>
       </div>
-      <button onClick={()=> removetHandler(productId)}><FaTrash/></button>
+
+      <button onClick={() => removeHandler(productId)}>
+        <FaTrash />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default CartItem
-
+export default CartItem;
